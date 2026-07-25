6770363dfd59303c78bc2c6b7b21770d4eb33c4b
aaronkigun <aaron.kigun@binghamuni.edu.ng>
2026-07-25 20:30:28 +0100

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   dikim-next/server.js

--- dikim-next/server.js
+++ dikim-next/server.js
@@ -1,6 +1,7 @@
 const { spawn } = require("child_process");
 
-const child = spawn("npx", ["next", "start"], {
+// Use 'node' directly to run the Next.js CLI instead of 'npx'
+const child = spawn("node", ["node_modules/next/dist/bin/next", "start"], {
   stdio: "inherit",
   shell: true,
   env: process.env,
